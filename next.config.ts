ffac81b28b5230f2bfae915f57ffac441051c888
anig1scur <anigiscur@gmail.com>
2024-10-30 22:24:53 +0800

    feat: deploy to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/fcitx5-theme-designer' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
